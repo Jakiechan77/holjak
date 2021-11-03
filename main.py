9d519aec3be7c23d6678665d017181e94d415523
jakiechan77 <rkswl210@naver.com>
2021-11-03 16:36:11 +0900

    commit on master

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ print("홀짝 게임을 시작합니다")
 
 import random
 a = random.randint(1,10)
-print(a)
-
-my = "짝" 
+my =input("짝 또는 홀을 입력하세요")
 print(my)
 
-dab = ""
+b = ""
 if a % 2 == 0:
-    print("짝")
-    dab = "짝"
+    print("결과는 짝")
+    b = "짝"
 else:
-    print("홀")
-    dab = "홀"
+    print("결과는 홀")
+    b = "홀"
 
-if my == dab:
+if my == b:
     print("맞다 통과")
 
 else:
-    print("빵!!")
+    print("틀림 탈락")
