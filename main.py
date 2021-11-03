7f02b8fba00f2c80cdc0c83f5abbb2874ef0cc86
jakiechan77 <rkswl210@naver.com>
2021-11-03 16:40:34 +0900

    4:40

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ print("오징어게임에 오신 것을 환영합니다")
 print("홀짝 게임을 시작합니다")
 
 import random
-a = random.randint(1,10)
-b =input("짝 또는 홀을 입력하세요")
+A = random.randint(1,10)
+B =input("짝 또는 홀을 입력하세요")
 print("입력된 값은")
-print(b)
+print(B)
 
-c = ""
+C = ""
 if a % 2 == 0:
     print("결과는 짝")
-    c = "짝"
+    C = "짝"
 else:
     print("결과는 홀")
-    c = "홀"
+    C = "홀"
 
-if b == c:
+if B == C:
     print("맞다 통과")
 
 else:
